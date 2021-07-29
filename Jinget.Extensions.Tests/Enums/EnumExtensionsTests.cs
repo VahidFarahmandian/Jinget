2a6209ade8552e89a658a4fa063456c168b8c5e1
Vahid Farahmandian <farahmandian2011@gmail.com>
2021-07-29 20:08:04 +0430

    enum extensions added

---
+++ Jinget.Extensions.Tests/Enums/EnumExtensionsTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Jinget.Extensions.Tests
+{
+    [TestClass()]
+    public class EnumExtensionsTests
+    {
+        enum ProgrammingLanguage
+        {
+            CSharp,
+
+            [System.ComponentModel.Description("F#.Net")]
+            FSharp,
+            VB
+        }
+
+        [TestMethod()]
+        public void should_return_enum_field_value()
+        {
+            ProgrammingLanguage langauge = ProgrammingLanguage.CSharp;
+            string expected = "CSharp";
+            var result = langauge.GetDescription();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void should_return_enum_field_description_value()
+        {
+            ProgrammingLanguage langauge = ProgrammingLanguage.FSharp;
+            string expected = "F#.Net";
+            string result = langauge.GetDescription();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void should_return_enum_field_using_description()
+        {
+            string enumDescription = "F#.Net";
+            ProgrammingLanguage expected = ProgrammingLanguage.FSharp;
+
+            ProgrammingLanguage result = EnumExtensions.GetValueFromDescription<ProgrammingLanguage>(enumDescription);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void should_return_enum_field_using_field_name()
+        {
+            string enumDescription = "VB";
+            ProgrammingLanguage expected = ProgrammingLanguage.VB;
+
+            ProgrammingLanguage result = EnumExtensions.GetValueFromDescription<ProgrammingLanguage>(enumDescription);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.ComponentModel.InvalidEnumArgumentException))]
+        public void should_generate_InvalidEnumArgumentException()
+        {
+            string enumDescription = "Java";
+            EnumExtensions.GetValueFromDescription<ProgrammingLanguage>(enumDescription);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void should_generate_InvalidOperationException()
+        {
+            string enumDescription = "Java";
+            EnumExtensions.GetValueFromDescription<BaseData.InvalidStruct>(enumDescription);
+        }
+    }
+}
---
+++ Jinget.Extensions/Enums/EnumExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Jinget.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get Description of a specific enum value
+        /// If no Description is set on a field, then the stringfied value will be returned
+        /// </summary>
+        public static string GetDescription(this Enum value)
+        {
+            var fi = value.GetType().GetField(value.ToString());
+            if (fi == null)
+                return string.Empty;
+
+            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
+        }
+
+        /// <summary>
+        /// Return enum value based on the given Description. 
+        /// If no Description is set on a field, then field name will be compared aginst the given description
+        /// </summary>
+        /// <typeparam name="TEnum">typeof enum</typeparam>
+        public static TEnum GetValueFromDescription<TEnum>(string description) where TEnum : struct, IConvertible
+        {
+            var type = typeof(TEnum);
+            if (!type.IsEnum)
+                throw new InvalidOperationException();
+
+            foreach (var field in type.GetFields())
+            {
+                //Is there any Description attribute set for the field?
+                if (
+                    string.Equals(
+                        Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute ? attribute.Description : field.Name,
+                        description,
+                        StringComparison.CurrentCultureIgnoreCase)
+                    )
+                    return (TEnum)field.GetValue(null);
+            }
+
+            throw new InvalidEnumArgumentException($"Enum member with description/name '{description}', not found!");
+        }
+    }
+}
