4459019399f1656c1946b4b95780f8058df07bb8
Vahid Farahmandian <farahmandian2011@gmail.com>
2021-07-30 18:27:05 +0430

    DbTypeExtensionsTests.cs added

---
+++ Jinget.Extensions.Tests/DbTypes/DbTypeExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+
+namespace Jinget.Extensions.DbTypes.Tests
+{
+    [TestClass()]
+    public class DbTypeExtensionsTests
+    {
+        [TestMethod()]
+        public void Should_return_true_for_numeric_dbType() => Assert.IsTrue(DbType.Int32.IsNumericType());
+
+        [TestMethod()]
+        public void Should_return_false_for_non_numeric_dbType() => Assert.IsFalse(DbType.Binary.IsNumericType());
+
+        [TestMethod()]
+        public void Should_return_true_for_unicode_dbType_IsUnicodeType() => Assert.IsTrue(DbType.String.IsUnicodeType());
+
+        [TestMethod()]
+        public void Should_return_false_for_non_unicode_dbType_IsUnicodeType() => Assert.IsFalse(DbType.AnsiString.IsUnicodeType());
+
+        [TestMethod()]
+        public void Should_return_false_for_non_unicode_dbType_IsNonUnicodeType() => Assert.IsFalse(DbType.String.IsNonUnicodeType());
+
+        [TestMethod()]
+        public void Should_return_true_for_non_unicode_dbType_IsNonUnicodeType() => Assert.IsTrue(DbType.AnsiString.IsNonUnicodeType());
+    }
+}
