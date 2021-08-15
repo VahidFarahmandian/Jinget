6d62928b8e428f01cf390ec45eb00ac231e1e239
Vahid Farahmandian <farahmandian2011@gmail.com>
2021-08-15 12:04:18 +0430

    string extensions added
    apply ye ke replacement for string and IDbCommand

---
+++ Jinget.Core/ExtensionMethods/Database/SqlClient/IDbCommandExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+
+namespace Jinget.Core.ExtensionMethods.Database.SqlClient
+{
+    public static class IDbCommandExtensions
+    {
+        /// <summary>
+        /// Replace Arabic ي and ك characters in CommandText and Parameters with their Farsi equalivants
+        /// </summary>
+        public static void ApplyCorrectYeKe(this IDbCommand command)
+        {
+            command.CommandText = command.CommandText.ApplyCorrectYeKe();
+
+            foreach (IDataParameter parameter in command.Parameters)
+            {
+                if (parameter.Value is DBNull)
+                    continue;
+
+                switch (parameter.DbType)
+                {
+                    case DbType.AnsiString:
+                    case DbType.AnsiStringFixedLength:
+                    case DbType.String:
+                    case DbType.StringFixedLength:
+                    case DbType.Xml:
+                        parameter.Value = parameter.Value.ToString().ApplyCorrectYeKe();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Jinget.Core/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Jinget.Core.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        static readonly char[] ArabicYeChar = { (char)1610, 'ي'};
+        const char PersianYeChar = (char)1740;
+
+        static readonly char[] ArabicKeChar = { (char)1603, 'ك' };
+        const char PersianKeChar = (char)1705;
+
+        /// <summary>
+        /// Replace Arabic ي and ك characters with their Farsi equalivants
+        /// </summary>
+        public static string ApplyCorrectYeKe(this string data)
+        {
+            if (string.IsNullOrWhiteSpace(data))
+                return string.Empty;
+
+            data = ArabicYeChar.Aggregate(data, (current, ye) => current.Replace(ye, PersianYeChar).Trim());
+
+            data = ArabicKeChar.Aggregate(data, (current, ke) => current.Replace(ke, PersianKeChar).Trim());
+
+            return data;
+        }
+    }
+}
