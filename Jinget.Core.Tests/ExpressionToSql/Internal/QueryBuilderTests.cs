dcd76fea26c38609e06774dfbc697ecdf3bbe8c9
Vahid Farahmandian <farahmandian2011@gmail.com>
2021-12-13 17:46:36 +0330

    query builder added

---
+++ Jinget.Core.Tests/ExpressionToSql/Internal/QueryBuilderTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace Jinget.Core.ExpressionToSql.Internal.Tests
+{
+    [TestClass()]
+    public class QueryBuilderTests
+    {
+        private QueryBuilder _qb;
+
+        [TestInitialize]
+        public void Initialize() => _qb = new QueryBuilder(new StringBuilder());
+
+        [TestMethod()]
+        public void should_return_top_expression()
+        {
+            string expectedResult = "SELECT TOP 10";
+            var result = _qb.Take(10).ToString();
+            Assert.AreEqual(expectedResult, result.ToString());
+        }
+
+        [TestMethod()]
+        public void should_return_columns()
+        {
+            string expectedResult = "SELECT c.[Col1], c.[Col2]";
+            var result = _qb.AddAttribute("Col1", "c").AddSeparator().AddAttribute("[Col2]", "c");
+            Assert.AreEqual(expectedResult, result.ToString());
+        }
+
+        [TestMethod()]
+        public void should_return_table_name()
+        {
+            string expectedResult = "SELECT FROM [Sch].[MyTable] AS T";
+            var result = _qb.AddTable(new Table
+            {
+                Name = "MyTable",
+                Schema = "Sch"
+            }, "T");
+            Assert.AreEqual(expectedResult, result.ToString());
+        }
+
+        [TestMethod()]
+        public void should_return_function_name()
+        {
+            string expectedResult = "SELECT FROM [Sch].[Function]() AS F";
+            var result = _qb.AddTable(new Table
+            {
+                Name = "Function()",
+                Schema = "Sch"
+            }, "F");
+            Assert.AreEqual(expectedResult, result.ToString());
+        }
+    }
+}
