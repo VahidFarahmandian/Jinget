0564c524357c0f4348ecf26aa0266a95329e73d4
Vahid Farahmandian <farahmandian2011@gmail.com>
2021-12-13 16:38:00 +0330

    expression to sql, paging added

---
+++ Jinget.Core.Tests/ExpressionToSql/Internal/PagingTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jinget.Core.ExpressionToSql.Internal.Tests
+{
+    [TestClass()]
+    public class PagingTests
+    {
+        [TestMethod()]
+        public void should_return_paging_clause()
+        {
+            string expectedResult = "OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY";
+            string result = new Paging { PageNumber = 1, PageSize = 5 }.ToString();
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod()]
+        public void should_return_paging_clause_default_page_size()
+        {
+            string expectedResult = "OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY";
+            string result = new Paging { PageNumber = -1, PageSize = -1 }.ToString();
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod()]
+        public void should_return_nothing()
+        {
+            string expectedResult = "";
+            string result = new Paging { PageNumber = 0, PageSize = 0 }.ToString();
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
