ef5e7448774d03180bf3ec500f749d14b348271b
Vahid Farahmandian <farahmandian2011@gmail.com>
2023-07-15 13:01:12 +0330

    Generate random string added to string utility

---
+++ 01-Core/Jinget.Core/Utilities/StringUtility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Jinget.Core.Utilities
+{
+    public static class StringUtility
+    {
+        /// <summary>
+        /// Create a random string with given <see cref="length"/> using given <see cref="characterSet"/>
+        /// </summary>
+        public static string GetRandomString(int length, IEnumerable<char> characterSet)
+        {
+            if (length < 0)
+                throw new ArgumentException("Jinget Says: length must not be negative", nameof(length));
+            if (length > int.MaxValue / 8) // 250 million chars ought to be enough for anybody
+                throw new ArgumentException("Jinget Says: length is too big", nameof(length));
+            if (characterSet == null)
+                throw new ArgumentNullException(nameof(characterSet));
+            var characterArray = characterSet.Distinct().ToArray();
+            if (characterArray.Length == 0)
+                throw new ArgumentException("Jinget Says: characterSet must not be empty", nameof(characterSet));
+
+            var bytes = new byte[length * 8];
+            var result = new char[length];
+            using (var cryptoProvider = RandomNumberGenerator.Create())
+            {
+                cryptoProvider.GetBytes(bytes);
+            }
+            for (int i = 0; i < length; i++)
+            {
+                ulong value = BitConverter.ToUInt64(bytes, i * 8);
+                result[i] = characterArray[value % (uint)characterArray.Length];
+            }
+            return new string(result);
+        }
+    }
+}
---
+++ Tests/Jinget.Core.Tests/Utilities/StringUtilityTests.cs
@@ -0,0 +1,23 @@
+﻿using Jinget.Core.Tests._BaseData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jinget.Core.Utilities.Tests
+{
+    [TestClass()]
+    public class StringUtilityTests
+    {
+        [TestMethod()]
+        public void should_create_random_string_using_given_characterset()
+        {
+            int length = 5;
+            IEnumerable<char> charset = new char[] { 'a', 'b', 'c', '1' };
+
+            var result = StringUtility.GetRandomString(length, charset);
+
+            Assert.IsTrue(result.Length == length);
+            Assert.IsTrue(result.ToCharArray().All(x => charset.Contains(x)));
+        }
+    }
+}
