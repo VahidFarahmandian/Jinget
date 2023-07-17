6f0c224a8e78b2483937e1837d4394635d572537
Vahid Farahmandian <farahmandian2011@gmail.com>
2023-07-17 18:17:32 +0330

    paging extensions test added

--- Tests/Jinget.Core.Tests/ExtensionMethods/ExpressionToSql/PagingExtensionsTests.cs
+++ Tests/Jinget.Core.Tests/ExtensionMethods/ExpressionToSql/PagingExtensionsTests.cs
@@ -6,20 +6,46 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Jinget.Core.ExtensionMethods.ExpressionToSql
 {
     [TestClass()]
-    public class OrderByExtensionsTests
+    public class PagingExtensionsTests
     {
         [TestMethod()]
         public void should_return_empty_string()
         {
+            Paging paging = null;
             List<OrderBy> lstOrderBy = null;
 
-            var result = lstOrderBy.GetSorting();
+            var result = paging.GetPaging(lstOrderBy);
 
             Assert.IsTrue(string.IsNullOrWhiteSpace(result));
         }
+
         [TestMethod()]
         public void should_return_stringfied_order_by_clause()
         {
+            Paging paging = null;
+            List<OrderBy> lstOrderBy = new List<OrderBy>
+            {
+                new OrderBy
+                {
+                    Name = x=>((TestClass)x).Property1,
+                    Direction = Enumerations.OrderByDirection.Descending
+                }
+            };
+            string expectedResult = "ORDER BY [Property1] DESC ";
+
+            var result = paging.GetPaging(lstOrderBy);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod()]
+        public void should_return_stringfied_paging_clause()
+        {
+            Paging paging = new Paging()
+            {
+                PageNumber = 1,
+                PageSize = 10
+            };
             List<OrderBy> lstOrderBy = new List<OrderBy>
             {
                 new OrderBy
@@ -34,9 +60,9 @@ namespace Jinget.Core.ExtensionMethods.ExpressionToSql
                 }
             };
 
-            string expectedResult = "ORDER BY [Property1] DESC,[Property2] ASC";
+            string expectedResult = "ORDER BY [Property1] DESC,[Property2] ASC OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY";
 
-            var result = lstOrderBy.GetSorting();
+            var result = paging.GetPaging(lstOrderBy);
 
             Assert.AreEqual(expectedResult, result);
         }
