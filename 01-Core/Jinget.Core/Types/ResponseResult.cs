5d042472f24b853b857981c522483b0b73c63da5
vahid <vahid@example.com>
2023-09-28 03:58:46 +0330

    IEnumerable added to ResponseResult

--- 01-Core/Jinget.Core/Types/ResponseResult.cs
+++ 01-Core/Jinget.Core/Types/ResponseResult.cs
@@ -1,18 +1,23 @@
-﻿namespace Jinget.Core.Types
+﻿using System.Collections.Generic;
+
+namespace Jinget.Core.Types
 {
     public class ResponseResult<TResponseModel>
-        where TResponseModel : new()
     {
         private static ResponseResult<TResponseModel>? _instance;
         public static ResponseResult<TResponseModel> Empty => _instance ??= new ResponseResult<TResponseModel>();
         public long EffectedRowsCount { get; }
 
-        public TResponseModel Data { get; }
+        public List<TResponseModel> Data { get; }
 
-        public ResponseResult() { Data = new TResponseModel(); }
+        public ResponseResult() { Data = new List<TResponseModel>(); }
 
-        public ResponseResult(TResponseModel data) : this() => Data = data;
+        public ResponseResult(TResponseModel data) : this() => Data.Add(data);
+        public ResponseResult(IEnumerable<TResponseModel> data) : this() => Data.AddRange(data);
+
 
         public ResponseResult(TResponseModel data, long effectedRowsCount) : this(data) => EffectedRowsCount = effectedRowsCount;
+        public ResponseResult(IEnumerable<TResponseModel> data, long effectedRowsCount) : this(data) => EffectedRowsCount = effectedRowsCount;
+
     }
 }
