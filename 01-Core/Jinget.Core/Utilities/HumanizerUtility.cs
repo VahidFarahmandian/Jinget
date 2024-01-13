b013eb92ad0a5fe22d690a0ac63a267fe0e2a480
مهرداد تابش <mehrdadit12@gmail.com>
2024-01-12 23:22:57 -0800

    feature : added humanizer utility for readable code

---
+++ 01-Core/Jinget.Core/Utilities/HumanizerUtility.cs
@@ -0,0 +1,11 @@
+﻿namespace Jinget.Core.Utilities
+{
+    public static class HumanizerUtility
+    {
+        public static bool Not(bool possibility)
+        {
+            return !possibility;
+        }
+
+    }
+}
