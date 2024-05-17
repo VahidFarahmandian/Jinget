c15a48d12095b6a062d32bd25acad209005e6106
vahid <vahid@example.com>
2024-05-18 00:38:19 +0330

    BaseTypeModel members are not required any more and they are optional

--- 04-Presentation/Jinget.Blazor/Models/BaseTypeModel.cs
+++ 04-Presentation/Jinget.Blazor/Models/BaseTypeModel.cs
@@ -2,9 +2,9 @@
 
 public class BaseTypeModel<TCode>
 {
-    public virtual required TCode Code { get; set; }
+    public virtual TCode? Code { get; set; }
 
-    public virtual required string Title { get; set; }
+    public virtual string? Title { get; set; }
 }
 public class BaseTypeModel : BaseTypeModel<byte>
 {
