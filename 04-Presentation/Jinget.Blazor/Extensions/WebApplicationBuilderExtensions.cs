1c131afa5d7698d951d3cf5e5392c755a302ea8a
vahid <vahid@example.com>
2024-05-29 02:51:27 +0330

    some improvements

--- 04-Presentation/Jinget.Blazor/Extensions/WebApplicationBuilderExtensions.cs
+++ 04-Presentation/Jinget.Blazor/Extensions/WebApplicationBuilderExtensions.cs
@@ -10,15 +10,15 @@ public static class WebApplicationBuilderExtensions
     {
         if (addComponents)
             services.AddMudServices();
+        services.Configure<TokenConfigModel>(x =>
+        {
+            x.Secret = tokenConfigModel.Secret;
+            x.ExpirationInMinute = tokenConfigModel.ExpirationInMinute;
+            x.TokenName = tokenConfigModel.TokenName;
+        });
+        
         if (tokenConfigModel != null)
         {
-            services.Configure<TokenConfigModel>(x =>
-            {
-                x.Secret = tokenConfigModel.Secret;
-                x.ExpirationInMinute = tokenConfigModel.ExpirationInMinute;
-                x.TokenName = tokenConfigModel.TokenName;
-            });
-
             services.TryAddScoped<ILocalStorageService, LocalStorageService>();
             services.TryAddScoped<ITokenStorageService>(
                 provider => new TokenStorageService(provider.GetRequiredService<ILocalStorageService>(), tokenConfigModel.TokenName));
