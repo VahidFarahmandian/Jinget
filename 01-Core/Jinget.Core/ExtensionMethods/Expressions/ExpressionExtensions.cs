a7e1bbdef27e82eff77b953ed34dd83a5ab6e6a4
vahid <vahid@example.com>
2024-09-26 19:29:10 +0330

    typo resolved

--- 01-Core/Jinget.Core/ExtensionMethods/Expressions/ExpressionExtensions.cs
+++ 01-Core/Jinget.Core/ExtensionMethods/Expressions/ExpressionExtensions.cs
@@ -3,8 +3,8 @@
 public static class ExpressionExtensions
 {
     /// <summary>
-    /// Transform anonymouse expression to non-anonymouse expression
-    /// This method is useful wherever the class's default constructor is not accessable due to its access level
+    /// Transform anonymous expression to non-anonymous expression
+    /// This method is useful wherever the class's default constructor is not accessible due to its access level
     /// but you need to use this constructor to define your expression
     /// </summary>
     public static Expression<Func<T, T>> Transform<T>(this Expression<Func<T, object>> source)
