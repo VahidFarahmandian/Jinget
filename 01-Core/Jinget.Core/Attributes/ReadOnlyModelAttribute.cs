a0cf236d6e3a1cc35a4a749f820e5bdb5711a25c
vahid <vahid@example.com>
2024-10-15 01:20:45 +0330

    ReadOnlyModelAttribute added for CQRS ease of use

---
+++ 01-Core/Jinget.Core/Attributes/ReadOnlyModelAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Jinget.Core.Attributes;
+
+/// <summary>
+/// Used to mark readonly models used for CQRS(While working with IReadOnlyRepository)
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class ReadOnlyModelAttribute : Attribute
+{
+}
