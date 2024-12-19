b76dee39c691a02f8a203091b158b20b72ef8fe9
vahid <vahid@example.com>
2024-12-19 17:12:33 +0330

    MapTo added to ResponseResult

--- 01-Core/Jinget.Core/Types/ResponseResult.cs
+++ 01-Core/Jinget.Core/Types/ResponseResult.cs
@@ -1,10 +1,11 @@
-﻿namespace Jinget.Core.Types;
+﻿using Mapster;
+
+namespace Jinget.Core.Types;
 
 public class ResponseResult<TResponseModel>
 {
     private static ResponseResult<TResponseModel>? _instance;
     public static ResponseResult<TResponseModel> Empty => _instance ??= new ResponseResult<TResponseModel>();
-    public long EffectedRowsCount { get; }
 
     /// <summary>
     /// if the <typeparamref name="TResponseModel"></typeparamref> is <see cref="ProblemDetails"/> then false will be returned
@@ -18,6 +19,7 @@ public class ResponseResult<TResponseModel>
     /// </summary>
     public bool IsFailure => !IsSuccess;
 
+    public long EffectedRowsCount { get; }
     public List<TResponseModel> Data { get; }
 
     public ResponseResult()
@@ -25,12 +27,72 @@ public class ResponseResult<TResponseModel>
         Data = [];
     }
 
-    public ResponseResult(TResponseModel data) : this() => Data.Add(data);
-    public ResponseResult(IEnumerable<TResponseModel> data) : this() => Data.AddRange(data);
+    public ResponseResult(TResponseModel data) : this()
+    {
+        if (data == null)
+            Data.AddRange([]);
+        else
+            Data.Add(data);
+    }
+
+    public ResponseResult(IEnumerable<TResponseModel>? data) : this() => Data.AddRange(data ?? []);
 
     public ResponseResult(TResponseModel data, long effectedRowsCount) : this(data) =>
         EffectedRowsCount = effectedRowsCount;
 
     public ResponseResult(IEnumerable<TResponseModel> data, long effectedRowsCount) : this(data) =>
         EffectedRowsCount = effectedRowsCount;
+
+    /// <summary>
+    /// Maps a <see cref="ResponseResult`TSource`"/> to a <see cref="ResponseResult`TDestination`"/>.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source data.</typeparam>
+    /// <typeparam name="TDestination">The type of the destination data.</typeparam>
+    /// <param name="input">The input <see cref="ResponseResult{TSource}"/> to be mapped.</param>
+    /// <returns>A new <see cref="ResponseResult{TDestination}"/> with the mapped data.</returns>
+    public static ResponseResult<TDestination> MapTo<TDestination>(ResponseResult<TResponseModel> input)
+    {
+        if (input == null)
+        {
+            throw new ArgumentNullException(nameof(input), "Input cannot be null");
+        }
+
+        try
+        {
+            var mappedData = input.Data.Select(x => x.Adapt<TDestination>()).ToList();
+            return new ResponseResult<TDestination>(mappedData, input.EffectedRowsCount);
+        }
+        catch (Exception ex)
+        {
+            // Handle or log the exception as needed
+            throw new InvalidOperationException("An error occurred while mapping the data", ex);
+        }
+    }
+
+    ///// <summary>
+    ///// Creates a new <see cref="ResponseResult{TResponseModel}"/> object using <typeparamref name="TResponseModel"/>.
+    ///// </summary>
+    ///// <typeparam name="TResponseModel">The type of the response model.</typeparam>
+    ///// <param name="input">The input object to be converted into a <see cref="ResponseResult{TResponseModel}"/>.</param>
+    ///// <returns>A new <see cref="ResponseResult{TResponseModel}"/> object.</returns>
+    //public static ResponseResult<TResponseModel> New<TResponseModel>(object input, long? effectedRowsCount = null)
+    //{
+    //    if (input == null)
+    //    {
+    //        return new ResponseResult<TResponseModel>([], effectedRowsCount.HasValue == false ? 0 : effectedRowsCount.Value);
+    //    }
+
+    //    if (input is IEnumerable enumerable)
+    //    {
+    //        var list = enumerable.Cast<TResponseModel>().ToList();
+    //        return new ResponseResult<TResponseModel>(list, effectedRowsCount.HasValue == false ? list.Count : effectedRowsCount.Value);
+    //    }
+
+    //    if (input is TResponseModel responseModel)
+    //    {
+    //        return new ResponseResult<TResponseModel>([responseModel], effectedRowsCount.HasValue == false ? 1 : effectedRowsCount.Value);
+    //    }
+
+    //    throw new ArgumentException($"Input must be of type {typeof(TResponseModel)} or IEnumerable of {typeof(TResponseModel)}", nameof(input));
+    //}
 }
--- Tests/Jinget.Core.Tests/Types/ResponseResultTests.cs
+++ Tests/Jinget.Core.Tests/Types/ResponseResultTests.cs
@@ -34,4 +34,89 @@ public class ResponseResultTests
         Assert.IsFalse(obj.IsSuccess);
         Assert.IsTrue(obj.IsFailure);
     }
+
+    [TestMethod()]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void should_throw_exception_for_null_input_mapto()
+    {
+        ResponseResult<SampleModel>.MapTo<SampleViewModel>(null);
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void should_throw_exception_for_incompatibale_types_mapto()
+    {
+        var source = new ResponseResult<SampleModel>([new SampleModel { Id = 1, Name = "Vahid" }], 1000);
+        ResponseResult<SampleModel>.MapTo<SampleInterfaceClass>(source);
+    }
+
+    [TestMethod()]
+    public void should_map_source_data_type_to_destination_data_type()
+    {
+        var source = new ResponseResult<SampleModel>(
+            [new SampleModel { Id = 1, Name = "Vahid" }, new SampleModel { Id = 2, Name = "Ali" }], 1000);
+        var result = ResponseResult<SampleModel>.MapTo<SampleViewModel>(source);
+
+        Assert.AreEqual(result.Data.GetType(), typeof(List<SampleViewModel>));
+        Assert.AreEqual(result.Data.First().Name, "Vahid");
+        Assert.AreEqual(result.Data.Last().Name, "Ali");
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsFalse(result.IsFailure);
+        Assert.AreEqual(result.EffectedRowsCount, 1000);
+    }
+
+    [TestMethod()]
+    public void should_create_new_responseResultObject_using_T()
+    {
+        SampleModel obj = new() { Id = 1, Name = "Vahid" };
+
+        var result = new ResponseResult<SampleModel>(obj, 1);
+
+        Assert.AreEqual(result.Data.GetType(), typeof(List<SampleModel>));
+        Assert.IsTrue(result.EffectedRowsCount == 1);
+        Assert.IsTrue(result.Data.First().Name == "Vahid");
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsFalse(result.IsFailure);
+    }
+
+    [TestMethod()]
+    public void should_create_new_responseResultObject_using_IEnumerableT()
+    {
+        List<SampleModel> obj = [new SampleModel { Id = 1, Name = "Vahid" }, new SampleModel { Id = 2, Name = "John" }];
+
+        var result = new ResponseResult<SampleModel>(obj, 2);
+
+        Assert.AreEqual(result.Data.GetType(), typeof(List<SampleModel>));
+        Assert.IsTrue(result.EffectedRowsCount == 2);
+        Assert.IsTrue(result.Data.First().Name == "Vahid");
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsFalse(result.IsFailure);
+    }
+
+    [TestMethod()]
+    public void should_create_new_responseResultObject_using_null()
+    {
+        var result = new ResponseResult<SampleModel>((IEnumerable<SampleModel>)null);
+
+        Assert.AreEqual(result.Data.GetType(), typeof(List<SampleModel>));
+        Assert.IsTrue(result.EffectedRowsCount == 0);
+        Assert.IsTrue(result.Data.FirstOrDefault() is null);
+        Assert.IsTrue(result.Data.Count == 0);
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsFalse(result.IsFailure);
+    }
+
+    [TestMethod()]
+    public void should_handle_empty_collection()
+    {
+        List<SampleModel> emptyList = [];
+
+        var result = new ResponseResult<SampleModel>(emptyList);
+
+        Assert.AreEqual(result.Data.GetType(), typeof(List<SampleModel>));
+        Assert.IsTrue(result.EffectedRowsCount == 0);
+        Assert.IsTrue(result.Data.Count == 0);
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsFalse(result.IsFailure);
+    }
 }
