d98f9ed0827afd81d3e01315f69323a6f379d7fc
Vahid Farahmandian <farahmandian2011@gmail.com>
2025-04-19 20:06:39 +0330

    new types added

--- 01-Core/Jinget.Core/Types/JwtModel.cs
+++ 01-Core/Jinget.Core/Types/JwtModel.cs
@@ -7,6 +7,8 @@ public class JwtModel
     public string Issuer { get; set; }
     public string Audience { get; set; }
     public DateTime NotBefore { get; set; } = DateTime.Now;
+    public int ExpirationInMinute { get; set; } = 5;
+    public string TokenName { get; set; } = "jinget.token";
 }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
---
+++ 01-Core/Jinget.Core/Types/SwaggerAuthenticationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Jinget.Core.Types;
+
+public class SwaggerAuthenticationModel
+{
+    public string ApplicationTitle { get; set; }
+    public string Version { get; set; } = "v1";
+    public string SchemeName { get; set; } = "Bearer";
+    public string SchemeDescription { get; set; } = "JWT Authorization header using the Bearer scheme. Example: \"jinget.token: Bearer {token}\"";
+}
