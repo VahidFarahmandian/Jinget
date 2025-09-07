546cce9b61534a653c3ddc9e044264650f4a08f1
Vahid Farahmandian <farahmandian2011@gmail.com>
2025-09-08 00:19:47 +0330

    temporaloptionmodel added

--- 01-Core/Jinget.Core/Types/QueryOption.cs
+++ 01-Core/Jinget.Core/Types/QueryOption.cs
@@ -35,5 +35,41 @@ public class QueryOption<TModelType> where TModelType : class, IEntity
         public bool ApplyTenantColumnFilter { get; set; } = true;
         public bool CalcTotalCount { get; set; } = true;
         public bool IgnoreAutoIncludes { get; set; } = true;
+        public TemporalOptionModel? TemporalOption { get; set; } = null;
+    }
+
+    public class TemporalOptionModel
+    {
+        public TemporalType Type { get; set; } = TemporalType.All;
+        public DateTime UtcFrom { get; set; }
+        public DateTime UtcTo { get; set; }
+
+        public enum TemporalType : byte
+        {
+            /// <summary>
+            /// Returns all rows in the historical data.
+            /// </summary>
+            All,
+
+            /// <summary>
+            /// Returns all rows that were active between two given UTC times.
+            /// </summary>
+            FromTo,
+
+            /// <summary>
+            /// The same as <seealso cref="FromTo"/>, except that rows are included that became active on the upper boundary.
+            /// </summary>
+            Between,
+
+            /// <summary>
+            /// Returns rows that were active (current) at the given UTC time. This is a single row from the history table for a given primary key.
+            /// </summary>
+            AsOf,
+
+            /// <summary>
+            /// Returns all rows that started being active and ended being active between two given UTC times.
+            /// </summary>
+            ContainedIn
+        }
     }
 }
