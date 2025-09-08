7d0956d1a0fc256961c6f4af85b0cd0521ca1bc0
Vahid Farahmandian <farahmandian2011@gmail.com>
2025-09-08 22:24:11 +0330

    comments added

--- 01-Core/Jinget.Core/Types/QueryOption.cs
+++ 01-Core/Jinget.Core/Types/QueryOption.cs
@@ -43,31 +43,41 @@ public class QueryOption<TModelType> where TModelType : class, IEntity
         public TemporalType Type { get; set; } = TemporalType.All;
         public DateTime UtcFrom { get; set; }
         public DateTime UtcTo { get; set; }
+        public DateTime UtcPointInTime { get; set; }
 
         public enum TemporalType : byte
         {
             /// <summary>
             /// Returns all rows in the historical data.
+            /// همه رکوردهای تاریخچه + رکورد فعلی رو برمی‌گردونه.
             /// </summary>
             All,
 
             /// <summary>
-            /// Returns all rows that were active between two given UTC times.
-            /// </summary>
-            FromTo,
-
-            /// <summary>
             /// The same as <seealso cref="FromTo"/>, except that rows are included that became active on the upper boundary.
+            /// هر رکوردی که حتی بخشی از بازه زمانی [from, to] رو پوشش بده برمی‌گردونه.
             /// </summary>
             Between,
 
             /// <summary>
+            /// Returns all rows that were active between two given UTC times.
+            /// مثل Between عمل می‌کنه ولی بازه زمانی رکوردها رو دقیقاً به [from, to] برش می‌زنه.
+            /// </summary>
+            FromTo,
+
+            /// <summary>
             /// Returns rows that were active (current) at the given UTC time. This is a single row from the history table for a given primary key.
+            /// وضعیت جدول در یک لحظه خاص از زمان رو نشون میده.
             /// </summary>
             AsOf,
 
             /// <summary>
             /// Returns all rows that started being active and ended being active between two given UTC times.
+            /// فقط رکوردهایی که کاملاً درون بازه زمانی [from, to] بودن.
+            /// اگر رکورد قبل از بازه شروع شده باشه یا بعد از بازه ادامه داشته باشه، حذف میشه.
+            /// بسیار شبیه FromTo هست
+            /// با این تفاوت که FromTo
+            /// تاربخ شروع و پایان رو در صورت خارج محدوده بودن، برش میزنه
             /// </summary>
             ContainedIn
         }
