07d6c6a965d16931900b40cc3a02d5b4f3f7fe12
Vahid Farahmandian <farahmandian2011@gmail.com>
2025-11-03 19:25:26 +0330

    StringCollectionValueObject added

---
+++ 01-Core/Jinget.Core/Types/ValueObject/StringCollectionValueObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Jinget.Core.Types.ValueObject;
+
+public class StringCollectionValueObject : JingetValueObject
+{
+    private StringCollectionValueObject()
+    {
+
+    }
+
+    [JsonConstructor]
+    public StringCollectionValueObject(ICollection<string> values) => Values = values;
+
+    public ICollection<string> Values { get; protected set; }
+
+}
